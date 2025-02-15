eeb0034dd5b7559c17c82f279ee0365d3dbd2e50
Envel-Nikita-Gutsenkov <52075465+Envel-Nikita-Gutsenkov@users.noreply.github.com>
2025-02-16 01:53:09 +0300

    Обновить behavior.ts

--- behavior.ts
+++ behavior.ts
@@ -1,23 +1,7 @@
 namespace myCustomBlocks {
     let lastItem: Item | null = null;
-    let lastComparison: ComparisonOperator | null = null;
+    let lastComparison: string | null = null;
     let lastCount: number | null = null;
-
-    /**
-     * Оператор сравнения (чтобы сделать выбор знака кликабельным)
-     */
-    enum ComparisonOperator {
-        //% block=">"
-        Greater = ">",
-        //% block="<"
-        Less = "<",
-        //% block=">="
-        GreaterOrEqual = ">=",
-        //% block="<="
-        LessOrEqual = "<=",
-        //% block="=="
-        Equal = "=="
-    }
 
     /**
      * Логическая проверка количества предметов
@@ -26,9 +10,9 @@ namespace myCustomBlocks {
      */
     //% block="проверить %item %comparison %count"
     //% item.shadow=itemPicker
-    //% comparison.defl=ComparisonOperator.GreaterOrEqual
+    //% comparison.shadow=dropdownComparison
     //% count.shadow=math_number defl=10
-    export function checkItemCount(item: Item, comparison: ComparisonOperator, count: number): boolean {
+    export function checkItemCount(item: Item, comparison: string, count: number): boolean {
         lastItem = item;
         lastComparison = comparison;
         lastCount = count;
@@ -41,9 +25,9 @@ namespace myCustomBlocks {
      */
     //% block="прекратить подачу если %expectedItem %expectedComparison %expectedCount"
     //% expectedItem.shadow=itemPicker
-    //% expectedComparison.defl=ComparisonOperator.GreaterOrEqual
+    //% expectedComparison.shadow=dropdownComparison
     //% expectedCount.shadow=math_number defl=10
-    export function stopItem(expectedItem: Item, expectedComparison: ComparisonOperator, expectedCount: number): void {
+    export function stopItem(expectedItem: Item, expectedComparison: string, expectedCount: number): void {
         if (lastItem === expectedItem && lastComparison === expectedComparison && lastCount === expectedCount) {
             player.say("Подача прекращена! Вы выбрали правильную комбинацию.");
         } else {
