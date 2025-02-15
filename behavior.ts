acdd3b10463dff36456ddfe271d68bbde81816a9
Envel-Nikita-Gutsenkov <52075465+Envel-Nikita-Gutsenkov@users.noreply.github.com>
2025-02-16 02:59:01 +0300

    Обновить behavior.ts

--- behavior.ts
+++ behavior.ts
@@ -12,24 +12,6 @@ namespace myCustomBlocks {
     //% item.shadow=itemPicker
     export function checkBlockCount(item: Item, count: number): number {
         lastItem = item;
-        return item === lastExpectedItem ? lastExpectedCount : 0;
-    }
-
-    /**
-     * Прекратить подачу
-     * Проверяет, правильно ли ребёнок собрал конструкцию в MD
-     * Ожидаемое значение передаётся скрыто через MD
-     */
-    //% block="прекратить подачу"
-    export function stopBlock(expectedItem: Item, expectedCount: number): void {
-        lastExpectedItem = expectedItem;
-        lastExpectedCount = expectedCount;
-
-        if (lastItem === expectedItem) {
-            player.say(`Подача прекращена! Ожидалось: ${expectedCount} ${expectedItem}.`);
-            blocks.place(Block.RedstoneBlock, world(0, 4, 0)); // Ставим блок красного камня в мире
-        } else {
-            player.say("Ошибка! Проверьте условия.");
-        }
+        return 0;
     }
 }
