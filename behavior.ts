9806caef4777581b0f9753eb272e2df1ea033fcf
Envel-Nikita-Gutsenkov <52075465+Envel-Nikita-Gutsenkov@users.noreply.github.com>
2025-02-16 03:21:28 +0300

    Обновить behavior.ts

--- behavior.ts
+++ behavior.ts
@@ -1,29 +1,29 @@
 namespace myCustomBlocks {
-    let lastBlock: Block | null = null;
-    let lastExpectedBlock: Block | null = null;
+    let lastItem: Item | null = null;
+    let lastExpectedItem: Item | null = null;
     let lastExpectedCount: number = 0;
 
     /**
-     * Логическая проверка количества блоков
+     * Логическая проверка количества предметов
      * Используется ВНУТРИ IF в MakeCode
-     * Запоминает, какой блок выбрал ребёнок
+     * Запоминает, какой предмет выбрал ребёнок
      */
-    //% block="количество %block"
-    //% block.shadow=minecraftBlock
-    export function getBlockCount(block: Block): number {
-        lastBlock = block;
-        return block === lastExpectedBlock ? lastExpectedCount : 0; // Если блок правильный, вернуть число из MD, иначе 0
+    //% block="количество %item"
+    //% item.shadow=minecraftItem
+    export function getItemCount(item: Item): number {
+        lastItem = item;
+        return item === lastExpectedItem ? lastExpectedCount : 0;
     }
 
     /**
      * Прекратить подачу
      * Проверяет, правильно ли ребёнок собрал конструкцию в MD
-     * Параметры скрыты в MD
+     * Ожидаемое значение передаётся скрыто через MD
      */
     //% block="прекратить подачу"
     export function stopBlock(): void {
-        if (lastBlock === lastExpectedBlock) {
-            player.say(`Подача прекращена! Ожидалось: ${lastExpectedCount} ${lastExpectedBlock}.`);
+        if (lastItem === lastExpectedItem) {
+            player.say(`Подача прекращена! Ожидалось: ${lastExpectedCount} ${lastExpectedItem}.`);
             blocks.place(Block.RedstoneBlock, world(0, 4, 0)); // Ставим блок красного камня в мире
         } else {
             player.say("Ошибка! Проверьте условия.");
